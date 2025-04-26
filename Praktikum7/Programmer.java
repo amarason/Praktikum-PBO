affb57f89d660626370242c5c340c3e71b05dbdb
sonamara <amaraputrisoniaji@students.undip.ac.id>
2025-04-27 00:35:56 +0700

    Praktikum 7

--- Praktikum7/Programmer.java
+++ Praktikum7/Programmer.java
@@ -1,6 +1,6 @@
 /* Nama File    : Progammer.java
  * Deskripsi    : berisi atribut dan method dalam subclass Programmer dengan konsep polimorfisme
- * Tanggal      : 25 April 2025
+ * Tanggal      : 24 April 2025
  * Nama/NIM     : Amara Putri Soniaji/24060123120004 
  */
 
