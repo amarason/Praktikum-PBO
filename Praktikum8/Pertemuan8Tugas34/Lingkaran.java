329df3866a5f1d20b76c77b68b43973fd790d27a
sonamara <amaraputrisoniaji@students.undip.ac.id>
2025-05-13 16:08:21 +0700

    Pertemuan 8

---
+++ Praktikum8/Pertemuan8Tugas34/Lingkaran.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan8tugas34;
+
+/**
+ *
+ * @author ACER
+ */
+
+/* Nama File: Lingkaran.java
+ * Deskripsi: implementasi Lingkaran sebagai BangunDatar
+ * Nama/NIM: Amara Putri Soniaji/ 24060123120004
+ * Tanggal: 10 Mei 2025
+*/
+
+public class Lingkaran extends BangunDatar {
+    private double jejari;
+    
+    public Lingkaran(double jejari){
+        this.jejari = jejari;
+    }
+    
+    public double getSisi(){
+        return jejari;
+    }
+    
+    @Override
+    public double hitungKeliling(){
+        return 2*jejari*3.14;
+    }   
+}
